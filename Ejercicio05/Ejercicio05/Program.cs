7b49e029aba8d5d7b0343fc05b175a177dc23e18
diego.taub <diego.taub@accenture.com>
2021-03-25 20:18:51 -0300

    se agregó otro tipo de resolución

--- Ejercicio05/Ejercicio05/Program.cs
+++ Ejercicio05/Ejercicio05/Program.cs
@@ -10,11 +10,21 @@ namespace Ejercicio05
     {
         static void Main(string[] args)
         {
+            /*ConsoleKeyInfo c;
+            do
+            {
+                c = Console.ReadKey();
+            } while (!c.Modifiers.HasFlag(ConsoleModifiers.Control) || c.Key.ToString().ToUpper() != "F");
+            */
             ConsoleKeyInfo c;
             do
             {
                 c = Console.ReadKey();
-            } while (c.Modifiers != ConsoleModifiers.Control || c.Key.ToString().ToUpper() != "F");
+                if((c.Key == ConsoleKey.F) && ((c.Modifiers & ConsoleModifiers.Control) != 0))
+                {
+                    break;
+                }
+            } while (true);
         }
     }
 }
